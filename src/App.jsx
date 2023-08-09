df9caef953a364892b6cd84d6c21e53a79d09831
Shahbaz <mshahbazramzan952@gmail.com>
2023-08-09 17:45:24 +0500

    grid using api

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 // import "./App.css";
 import App1_grid from "./App1_grid";
+import App2_grid from "./App2_grid";
 
 function App() {
   return (
     <>
       <h1>Ag Grid</h1>
-      <App1_grid />
+      {/* <App1_grid /> */}
+      <App2_grid />
     </>
   );
 }
